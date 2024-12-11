a2a9d859da21c857eb851bcc20f2c9f3e3c65060
JasonVranek <jasonvranek@gmail.com>
2024-12-11 19:40:56 +0000

    Issue 15 exclusion preconf (#17)
    * Copy over RLP and MerkleTrie libs from bolt
    
    * Add additional libraries from Bolt
    
    * Initial draft of exclusion preconf slasher contract
    
    * fix slashing logic
    
    Instead of using MerkleTrie.verifyExclusionProof, we simply have to prove that the transaction was included and then slash them for failing to exclude.
    
    * End to end test passing. Slash an operator on the URC for breaking an exclusion preconf promise

---
+++ example/PreconfStructs.sol
@@ -0,0 +1,52 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.0 <0.9.0;
+
+// Adapted from https://github.com/chainbound/bolt/tree/unstable/bolt-contracts
+
+contract PreconfStructs {
+    struct SignedCommitment {
+        uint64 slot;
+        bytes signature;
+        bytes signedTx;
+    }
+
+    struct TransactionData {
+        bytes32 txHash;
+        uint256 nonce;
+        uint256 gasLimit;
+    }
+
+    struct BlockHeaderData {
+        bytes32 parentHash;
+        bytes32 stateRoot;
+        bytes32 txRoot;
+        uint256 blockNumber;
+        uint256 timestamp;
+        uint256 baseFee;
+    }
+
+    struct AccountData {
+        uint256 nonce;
+        uint256 balance;
+    }
+
+    struct InclusionProof {
+        // block number where the transactions are included
+        uint256 inclusionBlockNumber;
+        // RLP-encoded block header of the previous block of the inclusion block
+        // (for clarity: `previousBlockHeader.number == inclusionBlockNumber - 1`)
+        bytes previousBlockHeaderRLP;
+        // RLP-encoded block header where the committed transactions are included
+        bytes inclusionBlockHeaderRLP;
+        // merkle inclusion proof of the account in the state trie of the previous block
+        // (checked against the previousBlockHeader.stateRoot)
+        bytes accountMerkleProof;
+        // merkle inclusion proof of the transactions in the transaction trie of the inclusion block
+        // (checked against the inclusionBlockHeader.txRoot). The order of the proofs should match
+        // the order of the committed transactions in the challenge: `Challenge.committedTxs`.
+        bytes[] txMerkleProofs;
+        // indexes of the committed transactions in the block. The order of the indexes should match
+        // the order of the committed transactions in the challenge: `Challenge.committedTxs`.
+        uint256[] txIndexesInBlock;
+    }
+}
